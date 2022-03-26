4a21d3d7a93b51bd140476b182e9c623b648e0c8
Ali Andrada <alidejando@gmail.com>
2022-03-26 21:31:21 +0800

    dumb env implementation

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  env: {
+    apiKey: "AIzaSyAxJL1Z5o9qNryRClchNvsX-yVEk2A_gtw",
+    authDomain: "tasktracker-d1d48.firebaseapp.com",
+    projectId: "tasktracker-d1d48",
+    storageBucket: "tasktracker-d1d48.appspot.com",
+    messagingSenderId: "259293364929",
+    appId: "1:259293364929:web:9d1c68632779823c71ca40"
+  }
 }
 
 module.exports = nextConfig
